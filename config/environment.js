c08abaec8e56780cbd04399789d2f3c0eab6a667
kagemusha <mmadrid99@gmail.com>
2014-08-09 11:38:05 -0400

    hook up adapter

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,7 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.SERVER = "http://localhost:3000";
     ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
