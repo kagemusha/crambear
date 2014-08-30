f6c061f2133edea51980a00e400a0e3b092ee9e8
kagemusha <mmadrid99@gmail.com>
2014-08-30 10:04:43 -0400

    upgrade to ember-cli 0.0.42

--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.SERVER = "http://localhost:3000";
   }
 
   if (environment === 'test') {
