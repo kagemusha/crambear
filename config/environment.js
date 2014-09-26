ce0acab36c4a9f4f3c4965fa124ecc849b07bf0d
Cory Forsyth <cory.forsyth@gmail.com>
2014-09-26 16:05:56 -0400

    updated to ember-cli 0.0.46

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
+    modulePrefix: 'crambear',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
@@ -25,11 +26,16 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.APP.SERVER = "http://localhost:3000";
   }
 
   if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'auto';
 
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
