106dba7b78999321b0909f91000b19f5ea56f4dd
kagemusha <mmadrid99@gmail.com>
2015-02-22 22:17:29 -0500

    upgrade to cli 0.2-beta1, ember 1.10

--- config/environment.js
+++ config/environment.js
@@ -20,21 +20,23 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.SERVER = "http://localhost:3000";
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
-    ENV.locationType = 'auto';
+    ENV.locationType = 'none';
 
+    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
+
     ENV.APP.rootElement = '#ember-testing';
   }
 
