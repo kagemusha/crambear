deb78a5e7b56f5b90f9830aa4d7535f235d4b733
kagemusha <mmadrid99@gmail.com>
2015-06-14 19:59:48 -0400

    fix adapters/serializers for current ember-data version

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,11 @@
+import Ember from 'ember';
 import JsonApiAdapter from 'ember-json-api/json-api-adapter';
 import config from '../config/environment';
 
 export default JsonApiAdapter.extend({
-  host: config.APP.SERVER
+  host: config.APP.SERVER,
+  pathForType(type) {
+    var underscored = Ember.String.underscore(type);
+    return Ember.String.pluralize(underscored);
+  }
 });
