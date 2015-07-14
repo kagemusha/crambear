3f4188d19a07388eeff017b5b8e3fc836a848298
kagemusha <mmadrid99@gmail.com>
2015-07-14 12:56:39 -0400

    fix async deprecation

--- app/models/card-set.js
+++ app/models/card-set.js
@@ -4,7 +4,7 @@ import Em from 'ember';
 var attr = DS.attr;
 
 export default DS.Model.extend({
-  cards: DS.hasMany("card"),
+  cards: DS.hasMany("card", {async: false}),
   labels: [], //todo: impl labels model as hasMany
   cardCount: Em.computed.alias("cards.length"),
   name: attr(),
