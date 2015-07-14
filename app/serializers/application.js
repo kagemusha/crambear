645fd94347ff6313198e7e46f967f3df52e80f34
kagemusha <mmadrid99@gmail.com>
2015-07-14 12:55:53 -0400

    fix serializer to handle underscores in attrs and relats

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -2,4 +2,12 @@ import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
   isNewSerializerAPI: true,
+
+  keyForAttribute: function(attr) {
+    return attr && attr.underscore();
+  },
+
+  keyForRelationship: function(rawKey) {
+    return rawKey && rawKey.underscore();
+  }
 });
