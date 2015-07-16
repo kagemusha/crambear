d02cc1e0d1b01b3effbf8ee75785d3145dea218a
kagemusha <mmadrid99@gmail.com>
2015-07-15 22:00:04 -0400

    modify to work with json-api-resources on server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,7 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
   host: config.APP.SERVER,
-  pathForType(type) {
-    var underscored = Ember.String.underscore(type);
-    return Ember.String.pluralize(underscored);
-  }
 });
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,13 +1,6 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
   isNewSerializerAPI: true,
-
-  keyForAttribute: function(attr) {
-    return attr && attr.underscore();
-  },
-
-  keyForRelationship: function(rawKey) {
-    return rawKey && rawKey.underscore();
-  }
 });
