5a3e8385da29ec163052c48de47c1d07d0cc2625
kagemusha <mmadrid99@gmail.com>
2015-07-30 14:34:27 -0400

    on study page, if no cards, prompt to create cards

--- app/routes/study.js
+++ app/routes/study.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(params) {
+  model(params) {
     return this.store.findRecord('cardSet', params.card_set_id);
   },
+
+  actions: {
+    gotoCardsetPage(cardSet){
+      this.transitionTo('card-set', cardSet );
+    }
+  }
 });
