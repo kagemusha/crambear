de2fd634f3ccdda6fe341b39f49705b0d024bb94
kagemusha <mmadrid99@gmail.com>
2015-08-03 13:17:06 -0400

    cleanup client storage

--- app/util/client-storage.js
+++ app/util/client-storage.js
@@ -1,44 +1,18 @@
-let inMemoryStorage = {};
-
 export default {
-  isActive: function() {
-    let t = 'test';
-
-    try {
-      window.localStorage.setItem(t, t);
-      return true;
-    } catch(err){
-      return false;
-    }
-  },
 
   set: function(key, value) {
-    if(!this.isActive()) {
-      inMemoryStorage[key] = value;
-      return;
-    }
-
     try {
-      window.localStorage.setItem(key, value);
+      window.localStorage.setItem(key, JSON.stringify(value));
     } catch(err) { }
   },
 
   get: function(key) {
-    if(!this.isActive()) {
-      return inMemoryStorage[key];
-    }
-
     try {
-      return window.localStorage.getItem(key);
+      return JSON.parse(window.localStorage.getItem(key));
     } catch(err) { }
   },
 
   remove: function(key) {
-    if(!this.isActive()) {
-      delete inMemoryStorage[key];
-      return;
-    }
-
     try {
       window.localStorage.removeItem(key);
     } catch(err) { }
