9f288c33b8f358a6fa12fa88133ce11bf43ef0a1
kagemusha <mmadrid99@gmail.com>
2015-08-12 21:31:37 -0400

    better clicking behavior and instructions for card tags

--- app/components/card-tags.js
+++ app/components/card-tags.js
@@ -2,4 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['card-tags'],
+  clicked: false,
+  click(e){
+    this.set('clicked', true);
+    if (e.target.tagName !== 'INPUT'){
+      let focusElement = Ember.$(e.target).next().find('input');
+      if (Ember.isBlank(focusElement)) {
+        focusElement =  this.$('input:last').focus();
+      }
+      focusElement.focus();
+    }
+  }
 });
