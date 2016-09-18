52d2b9965715a3a0e6e80bf56515b20e11bfdbf7
kagemusha <mmadrid99@gmail.com>
2016-09-18 14:02:08 -0400

    get cardset

--- app/routes/cardset.js
+++ app/routes/cardset.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import CardsetNotFound from 'crambear/mixins/cardset-not-found';
 
 export default Ember.Route.extend(CardsetNotFound, {
+
+  model(params) {
+    return this.store.findRecord('cardset', params.card_set_id);
+  },
+
 });
